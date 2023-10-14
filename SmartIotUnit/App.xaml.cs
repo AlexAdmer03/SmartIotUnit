6ef3b7b5e60212c73203d8d9bf10407337f65a75
alex admer <alex.admer03@gmail.com>
2023-10-14 09:58:53 +0200

    Fan_device done

--- SmartIotUnit/App.xaml.cs
+++ SmartIotUnit/App.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SmartIotUnit.Models.Devices;
+using SmartIotUnit.Services;
 using System.Windows;
 
 namespace SmartIotUnit
@@ -13,5 +12,33 @@ namespace SmartIotUnit
     /// </summary>
     public partial class App : Application
     {
+        public static IHost? host {  get; set; }
+
+        public App()
+        {
+            host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureServices((config, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                    services.AddSingleton(new DeviceConfiguration(config.Configuration.GetConnectionString("Device")!));
+                    services.AddSingleton<DeviceManager>();
+                    services.AddSingleton<NetworkManager>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await host!.StartAsync();
+
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
--- SmartIotUnit/MainWindow.xaml.cs
+++ SmartIotUnit/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using SmartIotUnit.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartIotUnit
 {
@@ -24,6 +12,11 @@ namespace SmartIotUnit
     {
         private readonly DeviceManager _deviceManager;
         private readonly NetworkManager _networkManager;
+
+        //public MainWindow()
+        //{
+            
+        //}
 
         public MainWindow(DeviceManager deviceManager, NetworkManager networkManager)
         {
